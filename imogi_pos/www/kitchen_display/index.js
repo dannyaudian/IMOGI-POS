4f79f0bcd8d7413f15d9d7ceca39c34aa5a16255
dannyaudian <danny.a.pratama@gmail.com>
2025-09-01 12:45:23 +0700

    feat: add display and editor pages

---
+++ imogi_pos/www/kitchen_display/index.js
@@ -0,0 +1,13 @@
+frappe.ready(function() {
+    if (!CURRENT_BRANCH) {
+        frappe.msgprint(__('Please select a branch'));
+        return;
+    }
+    if (DOMAIN !== 'Restaurant') {
+        frappe.msgprint(__('Page not available for this domain'));
+        return;
+    }
+    if (imogi_pos.kitchen_display) {
+        imogi_pos.kitchen_display.init();
+    }
+});
