a234bed738231949d50ab6d2cae42c1674b71b1e
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-09-11 17:44:35 +0700

    Use daily naming series for cashier sessions

--- imogi_pos/imogi_pos/doctype/cashier_device_session/cashier_device_session.py
+++ imogi_pos/imogi_pos/doctype/cashier_device_session/cashier_device_session.py
@@ -1,7 +1,9 @@
 from frappe.model.document import Document
 from frappe.model.naming import make_autoname
+from frappe.utils import now_datetime
 
 
 class CashierDeviceSession(Document):
     def autoname(self):
-        self.name = make_autoname("SHF-.YYYYMMDD.-.###")
+        date = now_datetime().strftime("%Y%m%d")
+        self.name = make_autoname(f"SHF-{date}-.###")
