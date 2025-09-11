f1c46b7dfcc4db2d7aa0581e73ae340d6715b7b3
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-09-11 18:10:39 +0700

    Update index.js

--- imogi_pos/www/opening-balance/index.js
+++ imogi_pos/www/opening-balance/index.js
@@ -22,7 +22,7 @@ frappe.ready(() => {
   }
 
   // --- Render rows pecahan ---
-  const denominationValues = [100000, 50000, 20000, 10000, 5000, 2000, 1000, 500, 100];
+  const denominationValues = [100000, 50000, 20000, 10000, 5000, 2000];
   denominationValues.forEach((val) => {
     const tr = document.createElement('tr');
     tr.dataset.nominal = val;
