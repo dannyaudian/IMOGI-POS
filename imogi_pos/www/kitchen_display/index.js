8da8201ccbb5458a4cb94ea2288b3551e0ab2b17
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-09-16 11:54:34 +0700

    Update index.js

--- imogi_pos/www/kitchen_display/index.js
+++ imogi_pos/www/kitchen_display/index.js
@@ -1,22 +1,22 @@
-// frappe.ready(function() {
-//     if (imogi_pos.kitchen_display) {
-//         if (!CURRENT_BRANCH) {
-//             frappe.show_alert({
-//                 message: __('No branch selected. The Kitchen Display will determine the appropriate branch automatically.'),
-//                 indicator: 'orange',
-//             });
-//         }
+frappe.ready(function() {
+    if (imogi_pos.kitchen_display) {
+        if (!CURRENT_BRANCH) {
+            frappe.show_alert({
+                message: __('No branch selected. The Kitchen Display will determine the appropriate branch automatically.'),
+                indicator: 'orange',
+            });
+        }
 
-//         if (DOMAIN !== 'Restaurant') {
-//             frappe.show_alert({
-//                 message: __('Page not available for this domain'),
-//                 indicator: 'red',
-//             });
-//         }
+        if (DOMAIN !== 'Restaurant') {
+            frappe.show_alert({
+                message: __('Page not available for this domain'),
+                indicator: 'red',
+            });
+        }
 
-//         imogi_pos.kitchen_display.init();
-//     }
-// });
+        imogi_pos.kitchen_display.init();
+    }
+});
 
 /**
  * IMOGI POS - Kitchen Display
