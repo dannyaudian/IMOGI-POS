f773e525556fe310ef96924a6611a03eede3ef05
dannyaudian <danny.a.pratama@gmail.com>
2026-01-18 19:02:38 +0700

    fix pre build configuration

--- setup.py
+++ setup.py
@@ -3,26 +3,12 @@
 
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
 # get version from __version__ variable in imogi_pos/__init__.py
 from imogi_pos import __version__ as version
 
 setup(
     name="imogi_pos",
     version=version,
-    description="Point of Sale system for ERPNext",
-    author="IMOGI",
-    author_email="info@imogi.tech",
-    packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires,
-    license="MIT",
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 3.10",
-    ],
 )
